813629a466ce09590c611eba4e9f376637aa3cf6
sailorshy94 <sbutle94@gmail.com>
2024-01-09 19:36:05 -0500

    added app.use properties to allow the acceptance of JSON data, and to allow morgan to locally track and display server requests

--- server.js
+++ server.js
@@ -7,3 +7,33 @@ const fs = require('fs');
 // runs specific check for router # using nullish coalescence operator; will use for Heroku deployment
 const PORT = process.env.PORT ?? 3001;
 const app = express();
+
+// allows acceptance of json data (notes)
+app.use(express.json());
+// allows us to see requests made on server locally
+app.use(morgan('dev'));
+
+// TODO: 
+// GET /notes returns the notes.html file
+app.get('/notes', (req, res) =>{
+
+});
+
+// reads db.json file and returns saved notes as JSON
+app.get('/api/notes', (req, res) => {
+
+});
+
+// will receive new note, save on req body, add to db.json file, and return new note to user
+// TODO: add npm package that will give each note unique id when saved
+app.post('/api/notes', (req, res) => {
+
+});
+
+// will redirect to html file
+app.get('*', (req, res) => {
+
+});
+
+// will listen for connections to PORT
+app.listen();
